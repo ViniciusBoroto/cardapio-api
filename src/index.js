845456e05a0453dc810b0e360ba175872e9e36d2
Vinicius <viniciusdosanjos63@gmail.com>
2024-05-14 17:09:41 -0300

    mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
+mongoose.connect('')
+
+const Item = mongoose.model('Item', {
+    name: String,
+    description: String,
+    price: Number,
+    image_url: String
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.post('/', async (req, res) => {
+    const item = new Item({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image_url: req.body.image_url
+    })
+    await item.save()
+    res.send(item)
+})
+
+app.get('/help', (req, res) => {
+    res.send('HELPPPPP')
 })
 
 
