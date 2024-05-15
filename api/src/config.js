d3d8a06dad8dd394565681b0f89e49c25fd6967e
ViniciusBoroto <viniciusdosanjos63@gmail.com>
2024-05-14 22:36:06 -0300

    frontend react project created

---
+++ api/src/config.js
@@ -0,0 +1,5 @@
+var config = {
+    connectionString: 'mongodb+srv://vezindb:VezinDB6363@loginauthentication.eqtdjy0.mongodb.net/?retryWrites=true&w=majority&appName=LoginAuthentication'
+}
+
+module.exports = config
